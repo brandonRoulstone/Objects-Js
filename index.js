668a39d910465769294a5887c5cb90139c25c931
Brandon Roulstone <brandonroulstone71@gmail.com>
2023-11-13 14:08:46 +0200

    commit

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll("#buttons button");
+    let operators = ["+", "-", "*", "/", "."];
 
-let display = document.getElementById('display');
-    let currentInput = '';
-    let btnEvaluate = document.querySelector('btn-btn-sign');
+    let currentInput = "";
 
-function appendToDisplay(value) {
-    currentInput += value;
-    display.value = currentInput;
-}
-
-function clearDisplay() {
-    currentInput = '';
-    display.value = '';
-}
-
-function calculateResult() {
-    try {
-        currentInput = eval(currentInput);
-        display.value = currentInput;
-    } 
-    catch (error) {
-        display.value = 'Error';
-    }
-}
-
-// let btnEvaluate = document.querySelector('btn-btn-sign');
-
-// btnEvaluate.addEventListener('click', (e) => {
-//     if(display.value = currentInput && display.value >= 2){
-//         display.value = ''
-//     }
-// })
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            const value = button.getAttribute("data-value");
+            
+            if (operators.includes(value)) {
+                // Check if the operator is already in the current input
+                if (currentInput.includes(value)) {
+                    return; // Operator already used, do nothing
+                }
+            }
+            
+            if (value === "=") {
+                try {
+                    currentInput = eval(currentInput);
+                    display.value = currentInput;
+                } catch (error) {
+                    display.value = "Error";
+                }
+            } else if (value === "C") {
+                currentInput = "";
+                display.value = "";
+                // Re-enable operators
+                operators.forEach(operator => {
+                    const operatorButton = document.querySelector(`[data-value="${operator}"]`);
+                    operatorButton.disabled = false;
+                });
+            } else {
+                currentInput += value;
+                display.value = currentInput;
+                if (operators.includes(value)) {
+                    // Disable the operator button
+                    button.disabled = false;
+                }
+            }
+        });
+    });
+});
