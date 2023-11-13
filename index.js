9006a806b10ef6db2d4026e8ecf44d6f5778a795
Brandon Roulstone <brandonroulstone71@gmail.com>
2023-11-13 14:12:41 +0200

    second commit

--- index.js
+++ index.js
@@ -6,37 +6,59 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentInput = "";
 
     buttons.forEach(button => {
+
         button.addEventListener("click", function () {
+
             const value = button.getAttribute("data-value");
             
             if (operators.includes(value)) {
-                // Check if the operator is already in the current input
+
+                // Checks if the operator is already displayed
+
                 if (currentInput.includes(value)) {
+
                     return; // Operator already used, do nothing
+
                 }
             }
             
             if (value === "=") {
+
                 try {
                     currentInput = eval(currentInput);
+
                     display.value = currentInput;
+
                 } catch (error) {
+
                     display.value = "Error";
+
                 }
             } else if (value === "C") {
+
                 currentInput = "";
+
                 display.value = "";
-                // Re-enable operators
+
+                // Re-initialize operators
                 operators.forEach(operator => {
+
                     const operatorButton = document.querySelector(`[data-value="${operator}"]`);
+
                     operatorButton.disabled = false;
+
                 });
+
             } else {
+
                 currentInput += value;
+
                 display.value = currentInput;
+
                 if (operators.includes(value)) {
-                    // Disable the operator button
+
                     button.disabled = false;
+                    
                 }
             }
         });
